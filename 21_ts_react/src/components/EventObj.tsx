1706c52b2824c3d12c8e257895c478e99516c3ff
yoonkyoungsoo <edward4992@naver.com>
2024-04-26 10:25:11 +0900

    sudy: eventobj

--- 21_ts_react/src/components/EventObj.tsx
+++ 21_ts_react/src/components/EventObj.tsx
@@ -3,6 +3,7 @@ export default function EventObj() {
     <>
       <h2>ts react에서 e 객체 타입</h2>
       <button onClick={() => alert("클릭")}>그냥 사용</button>
+      <button onClick={(event) => alert("클릭")}>바로 event 객체 사용</button>
     </>
   );
 }
--- 21_ts_react/src/components/PropsTypes.tsx
+++ 21_ts_react/src/components/PropsTypes.tsx
@@ -1,28 +1,39 @@
 interface Props {
   name: string;
-  age?: number; // age props를 옵셔널하게받을 수 있음
+  age?: number; // age props를 옵셔널하게 받을 수 있음
 }
 
-// export default function PropsTypes({ name, age }: Props) {
+export default function PropsTypes({ name, age }: Props) {
+  return (
+    <>
+      <h2>props로 전달받은 데이터의 타입 interface로 지정하기</h2>
+      <div>name: {name}</div>
+      {age && <div>age: {age}</div>}
+    </>
+  );
+}
+
+// export default function PropsTypes(props: Props) {
+//   const { name, age } = props;
+
 //   return (
 //     <>
 //       <h2>props로 전달받은 데이터의 타입 interface로 지정하기</h2>
 //       <div>name: {name}</div>
-
 //       <div>age: {age}</div>
 //     </>
 //   );
 // }
 
-export default function PropsType(props: Props) {
-  const name = props.name;
-  const age = props.age;
+// export default function PropsTypes(props: Props) {
+//   const name = props.name;
+//   const age = props.age;
 
-  return (
-    <>
-      <h2>props로 전달받은 데이터의 타입 interface로 지정하기</h2>
-      <div>name: {props.name}</div>
-      <div>age: {props.age}</div>
-    </>
-  );
-}
+//   return (
+//     <>
+//       <h2>props로 전달받은 데이터의 타입 interface로 지정하기</h2>
+//       <div>name: {name}</div>
+//       <div>age: {age}</div>
+//     </>
+//   );
+// }
